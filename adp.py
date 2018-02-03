9119d1f04759bac6a684ced9cbdcd5fad51e5a95
johannes EVIN <jo.evin@gmx.fr>
2018-02-03 20:37:18 +0100

    ajout de fusion_xy dans point

--- adp.py
+++ adp.py
@@ -1,9 +1,11 @@
 
 class point():
 	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-		self.poids = 1
+		self.x = float(x)
+		self.y = float(y)
+		self.poids = 1.0
 
 
-print("hello")
+	def fusion_xy(self, x ,y):
+		self.x = (self.x * self.poids + x) / (poids + 1)
+		self.y = (self.y * self.poids + y) / (poids + 1)
